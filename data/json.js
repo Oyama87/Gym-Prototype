b2cc5bc12649c4eda0c08746f4c957190b1a8157
Oyama87 <JustinTaylorG@Gmail.com>
2017-11-23 01:30:06 -0800

    text data and files

--- data/json.js
+++ data/json.js
@@ -1,1 +1,33 @@
-JSON
+var day = {
+  date: "",
+  sessions: {
+    s1: [
+      {
+        exerciseName: "",
+        category: "",
+        resistance: "",
+        volume: ""
+    },
+      {
+        exerciseName: "",
+        category: "",
+        resistance: "",
+        volume: ""
+      }
+    ],
+    s2: [
+      {
+        exerciseName: "",
+        category: "",
+        resistance: "",
+        volume: ""
+    },
+      {
+        exerciseName: "",
+        category: "",
+        resistance: "",
+        volume: ""
+      }
+    ]
+  }
+}
