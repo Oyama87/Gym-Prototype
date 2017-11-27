7b93b23c430d147169fbed9542d1de1bc5805017
Oyama87 <JustinTaylorG@Gmail.com>
2017-11-26 19:08:27 -0800

    Add extra layer to data structure (calendar array w/ day objects

--- data/json.js
+++ data/json.js
@@ -2,29 +2,31 @@
  *  The overall data structure to hold workout data
 */
 
-var day = {
-  "date": "",
-  "sessions": [// Array of workout session objects.
-    {// Session1 Object, first session in the array.
-      "sessionName": "", // Supplied by user and app features .
-      "sessionWork": [// Array of exercise set objects.
-        {// First set object with exercise, resistance, and rep data.
-          "exerciseName": "",
-          "exerciseCatg": "",
-          "exerciseLoad": 0,
-          "exerciseLoadUnit": "",
-          "exerciseReps": 0,
-          "exerciseRepUnits": ""
-        },
-        {//Second set object with exercise, resistance, and rep data.
-          "exerciseName": "",
-          "exerciseCatg": "",
-          "exerciseLoad": 0,
-          "exerciseLoadUnit": "",
-          "exerciseReps": 0,
-          "exerciseRepUnits": ""
-        }
-      ]//end array of sets
-    }//end first session object in array "sessions" of day object
-  ]//end of "sessions" array
-}//end of day object
+var calendar = [ // Array of day objects
+  {
+    "date": "",
+    "sessions": [// Array of workout session objects.
+      {// Session1 Object, first session in the array.
+        "sessionName": "", // Supplied by user and app features .
+        "sessionWork": [// Array of exercise set objects.
+          {// First set object with exercise, resistance, and rep data.
+            "exerciseName": "",
+            "exerciseCatg": "",
+            "exerciseLoad": 0,
+            "exerciseLoadUnit": "",
+            "exerciseReps": 0,
+            "exerciseRepUnits": ""
+          },
+          {//Second set object with exercise, resistance, and rep data.
+            "exerciseName": "",
+            "exerciseCatg": "",
+            "exerciseLoad": 0,
+            "exerciseLoadUnit": "",
+            "exerciseReps": 0,
+            "exerciseRepUnits": ""
+          }
+        ]//end array of sets
+      }//end first session object in array "sessions" of day object
+    ]//end of "sessions" array
+  }//end of day object
+]
