f39ae1578b149f64214c44e5af1a0a2d99831cae
Oyama87 <JustinTaylorG@Gmail.com>
2017-11-28 02:35:09 -0800

    new scripts to start processing data

--- data/json.js
+++ data/json.js
@@ -1,33 +1,32 @@
-var day = {
-  date: "",
-  sessions: {
-    s1: [
-      {
-        exerciseName: "",
-        category: "",
-        resistance: "",
-        volume: ""
-    },
-      {
-        exerciseName: "",
-        category: "",
-        resistance: "",
-        volume: ""
-      }
-    ],
-    s2: [
-      {
-        exerciseName: "",
-        category: "",
-        resistance: "",
-        volume: ""
-    },
-      {
-        exerciseName: "",
-        category: "",
-        resistance: "",
-        volume: ""
-      }
-    ]
-  }
-}
+/*
+ *  The overall data structure to hold workout data
+*/
+
+var calendar = [ // Array of day objects
+  {
+    "date": "",
+    "sessions": [// Array of workout session objects.
+      {// Session1 Object, first session in the array.
+        "sessionName": "", // Supplied by user and app features .
+        "sessionWork": [// Array of exercise set objects.
+          {// First set object with exercise, resistance, and rep data.
+            "exerciseName": "",
+            "exerciseCatg": "",
+            "exerciseLoad": 0,
+            "exerciseLoadUnit": "",
+            "exerciseReps": 0,
+            "exerciseRepUnits": ""
+          },
+          {//Second set object with exercise, resistance, and rep data.
+            "exerciseName": "",
+            "exerciseCatg": "",
+            "exerciseLoad": 0,
+            "exerciseLoadUnit": "",
+            "exerciseReps": 0,
+            "exerciseRepUnits": ""
+          }
+        ]//end array of sets
+      }//end first session object in array "sessions" of day object
+    ]//end of "sessions" array
+  }//end of day object
+]
