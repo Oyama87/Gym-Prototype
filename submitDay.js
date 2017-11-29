56a4047e58b5c666de88140e038613fd006f8444
Oyama87 <JustinTaylorG@Gmail.com>
2017-11-28 20:27:36 -0800

    refined to simple list of functions and global variables. scoping issues to be worked out later

--- submitDay.js
+++ submitDay.js
@@ -1,38 +1,58 @@
+//
+// Load current calendar month data from server into array named "calendar"
+//
+
 var today = {
   "date"=""//Use whatever date finding method available
   "sessions": [ {
-      "sessionName": "";// Placeholder for user input to be passed to.
-      "sessionWork": [ {
-        "exerciseName": "",      //
-        "exerciseCatg": "",      // Placeholders for user input to be passed to.
-        "exerciseLoad": 0,       //
-        "exerciseLoadUnit": "",
-        "exerciseReps": 0,
-        "exerciseRepUnits": ""
-        }
-      ]
+    "sessionName": "";// Placeholder for user input to be passed to.
+    "sessionWork": [ {
+    "exerciseName": "",      //
+    "exerciseCatg": "",      // Placeholders for user input to be passed to.
+    "exerciseLoad": 0,       //
+    "exerciseLoadUnit": "",
+    "exerciseReps": 0,
+    "exerciseRepUnits": ""
     }
+    ]
+  }
   ]
-}
-
 };
 
-function addSession() {
   var currentSession = {
     "sessionName": "";// Placeholder for user input to be passed to using jQuery
-    "sessionWork": [ {
+    "sessionWork": [ /*{
       "exerciseName": "",      //
       "exerciseCatg": "",      // Placeholders for user input to be passed to using jQuery
       "exerciseLoad": 0,       //
       "exerciseLoadUnit": "",
       "exerciseReps": 0,
       "exerciseRepUnits": ""
+  }*/
+    ]
   }
-}
-  today.sessions.push(currentSession);
-};
 
-function saveSession() {
+function addSet() {
+  var set = {
+    "exerciseName": "",      //
+    "exerciseCatg": "",      // Placeholders for user input to be passed to using jQuery
+    "exerciseLoad": 0,       //
+    "exerciseLoadUnit": "",
+    "exerciseReps": 0,
+    "exerciseRepUnits": ""
+    }
+    currentSession.sessionWork.push(set);
+  }
+
+function addSession() {
+  today.sessions.push(currentSession);
+}
+
+function addDay() {
+  calendar.push(today);
+}
+
+function saveDay() {
   /* Express code here:
    * Back end code to update server/database, replacing JSON file with
    * updated data from after the .push(). JSON file on server will have
